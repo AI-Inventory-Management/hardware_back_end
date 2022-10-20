7ce64386441dca35ec90350bc2d62d41b74d4e2d
joseangeldelangel10 <joseangeldelangel10@gmail.com>
2022-10-19 23:46:38 -0500

    server setup corrected

--- serverSetup.sh
+++ serverSetup.sh
@@ -1,13 +1,13 @@
 #! /usr/bin/env bash
 
-sudo apt get update
+sudo apt-get update
 
-sudo apt get install python3
+sudo apt-get install python3
 
-sudo apt get install python3-pip
+sudo apt-get install python3-pip
 
 sudo pip3 install flask
 
 sudo pip3 install virtualenv
 
-sudo apt get install nginx
+sudo apt-get install nginx
